4bd316670f86d369c120fd183591c04e60901d14
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-18 19:38:15 +0200

    deploying project skelaton to main file

---
+++ src/Pages/App.tsx
@@ -0,0 +1,14 @@
+import Message from '../Components/Message';
+import './App.css'
+
+
+function App(){
+  return <div className='center'>
+    <Message></Message>
+    <p className='purple-bold'>A Big Hello from Chahine Team.<br></br>Let's Do This!</p>
+  </div>
+  
+  
+}
+
+export default App;
