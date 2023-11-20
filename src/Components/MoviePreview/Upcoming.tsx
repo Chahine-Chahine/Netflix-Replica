d7c861d60d7e7af6cce30ec2fcc3f50404ac1887
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-20 21:53:14 +0200

    implemented the base card compounent for upcoming movies

---
+++ src/Components/MoviePreview/Upcoming.tsx
@@ -0,0 +1,24 @@
+import "./Upcoming.css"
+
+
+
+
+function Upcoming(){
+
+
+    return (
+        <>
+
+        <div className="upcoming-card">
+            <div className="upcoming-title">Avatar The Last Airbender </div>
+            <div className="upcoming-description">Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony — then everything changed. A live-action adaptation of Aang's story.
+</div>
+        </div>
+        </>
+    )
+}
+
+
+
+
+export default Upcoming;
--- src/Pages/MoviePreview/MoviePreview.tsx
+++ src/Pages/MoviePreview/MoviePreview.tsx
@@ -1,20 +1,20 @@
 import Discover from '../../Components/ApiHandler/Genre';
 import Popular from '../../Components/ApiHandler/Popular';
 import Video from '../../Components/ApiHandler/Videos';
+import Upcoming from '../../Components/MoviePreview/Upcoming';
 import '../App/App.css'
-
+import './MoviePreview.css'
 function MoviePreview() {
 
    
 
     return (
         <>
-            <div className='center'>
-                <p className='purple-bold'>Movie Preview Page.</p>
-            </div>
-            <Popular/>
+          
+            <Upcoming/>
+            {/* <Popular/>
             <Discover/>
-            <Video/>
+            <Video/> */}
         </>
 
     );
