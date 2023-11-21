df780b2ae01d359e489143e83a1494d274b6a3d5
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-21 02:52:32 +0200

    implemented comming soon section v1

--- src/Pages/MoviePreview/MoviePreview.tsx
+++ src/Pages/MoviePreview/MoviePreview.tsx
@@ -1,23 +1,22 @@
-import Discover from '../../Components/ApiHandler/Genre';
-import Popular from '../../Components/ApiHandler/Popular';
-import Video from '../../Components/ApiHandler/Videos';
-import Upcoming from '../../Components/MoviePreview/Upcoming';
-import '../App/App.css'
-import './MoviePreview.css'
+import Discover from "../../Components/ApiHandler/Genre";
+import Popular from "../../Components/ApiHandler/Popular";
+import Video from "../../Components/ApiHandler/Videos";
+import Upcoming from "../../Components/MoviePreview/Upcoming";
+import "../App/App.css";
+import moviePreviewStyles from "./MoviePreview.module.css";
 function MoviePreview() {
+  return (
+    <>
 
-   
 
-    return (
-        <>
-          
-            <Upcoming/>
-            <Popular/>
-            <Discover/>
-            <Video/>
-        </>
-
-    );
+      {/* Coming Soon Section */}
+      <div className={moviePreviewStyles.cardContainer}>
+        <div></div>
+        <Upcoming />
+        {/* Coming Soon Section */}
+      </div>
+    </>
+  );
 }
 
 export default MoviePreview;
