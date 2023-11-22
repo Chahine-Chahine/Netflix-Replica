709d7ac9eb5ab329b703d3cd5973915a36980bbe
haidaryazbek <haidaryazbek66@gmail.com>
2023-11-22 15:03:09 +0200

    Mapped datails section

--- src/Components/MoviePreview/MoreDetails.tsx
+++ src/Components/MoviePreview/MoreDetails.tsx
@@ -1,47 +1,50 @@
 import moreDetailsStyles from './MoreDetails.module.css'
 
 function MoreDetails(){
+
+    const cast=["Al Pacino",
+                "Steven Bauer",
+                "Michelle Pfeiffer",
+                "Mary Elizabeth Mastrantonio",
+                "Paul Shenar",
+                "Robert Loggia",
+                "Miriam Colon",
+                "F. Murray Abraham",
+                "Harris Yulin",
+                "Ángel Salazar",
+                "Pepe Serna"];
+
+    const Cast = cast.map((item) => (
+        <p className={moreDetailsStyles.castname}>{item}</p>
+      ));
+
+    const details=[{title: "Watch offline",
+                    text:"Download and watch everywhere you go."},
+                    {title: "Genres",
+                    text:"Classic Movies, Crime Movies, Drama Movies, Thriller Movies"},
+                    {title: "This movie is...",
+                    text:"Violent"},
+                    {title: "Audio",
+                    text:"English - Audio Description, English [Original], French"},
+                    {title: "Subtitles",
+                    text:"Arabic, English, French"}]
+
+        const Details = details.map((item) => (
+            <div className={moreDetailsStyles.detail}>
+            <p className={moreDetailsStyles.detailTitle}>{item.title}</p>
+            <p className={moreDetailsStyles.detailText}>{item.text}</p>
+        </div>
+        ));
+
     return(
         <div className={moreDetailsStyles.moreDetails}>
             <h1 className={moreDetailsStyles.moredetailsHeader}>More Details</h1>
             <div className={moreDetailsStyles.detailsContainer}>
-                <div className={moreDetailsStyles.detail}>
-                    <p className={moreDetailsStyles.detailTitle}>Watch offline</p>
-                    <p className={moreDetailsStyles.detailText}>Download and watch everywhere you go.</p>
-                </div>
-                <div className={moreDetailsStyles.detail}>
-                    <p className={moreDetailsStyles.detailTitle}>Genres</p>
-                    <p className={moreDetailsStyles.detailText}>Classic Movies, Crime Movies, Drama Movies, Thriller Movies</p>
-                </div>
-                <div className={moreDetailsStyles.detail}>
-                    <p className={moreDetailsStyles.detailTitle}>This movie is...</p>
-                    <p className={moreDetailsStyles.detailText}>Violent</p>
-                </div>
-                <div className={moreDetailsStyles.detail}>
-                    <p className={moreDetailsStyles.detailTitle}>Audio</p>
-                    <p className={moreDetailsStyles.detailText}>English - Audio Description, English [Original], French</p>
-                </div>
-                <div className={moreDetailsStyles.detail}>
-                    <p className={moreDetailsStyles.detailTitle}>Subtitles</p>
-                    <p className={moreDetailsStyles.detailText}>Arabic, English, French</p>
-                </div>
-
+                {Details}
             </div>
             <p className={moreDetailsStyles.castTitle}>Cast</p>
             <div className={moreDetailsStyles.detailsContainer}>
-                <p className={moreDetailsStyles.castname}>Al Pacino</p>
-                <p className={moreDetailsStyles.castname}>Steven Bauer</p>
-                <p className={moreDetailsStyles.castname}>Mary Elizabeth Mastrantonio</p>
-                <p className={moreDetailsStyles.castname}>Paul Shenar</p>
-                <p className={moreDetailsStyles.castname}>Robert Loggia</p>
-                <p className={moreDetailsStyles.castname}>Miriam Colon</p>
-                <p className={moreDetailsStyles.castname}>F. Murray Abraham</p>
-                <p className={moreDetailsStyles.castname}>Harris Yulin</p>
-                <p className={moreDetailsStyles.castname}>Ángel Salazar</p>
-                <p className={moreDetailsStyles.castname}>Pepe Serna</p>
-
-
-
+                {Cast}
             </div>
         </div>
     )
