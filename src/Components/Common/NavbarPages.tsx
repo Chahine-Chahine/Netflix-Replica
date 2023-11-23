c3902eca5ed4c8da3163c9669d14ec45960e56d8
noah305 <nourahmad305@hotmail.com>
2023-11-23 09:04:58 +0200

    Added routing links

--- src/Components/Common/NavbarPages.tsx
+++ src/Components/Common/NavbarPages.tsx
@@ -20,7 +20,7 @@ function NavBarPages() {
                 <Link to='../AllMoviesPage' className={navbarStyles.btnLogin}>
                     JOIN NOW
                 </Link>
-                <Link to={`../MoviePreview/${19}`} className={navbarStyles.btnSignIn}>
+                <Link to='../' className={navbarStyles.btnSignIn}>
                     SIGN IN
                 </Link>
             </div>
